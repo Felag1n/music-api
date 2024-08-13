a764a21c77a7daeb838fadd5011e5f72c3bc52dd
Felag1n <pipipopo876890767@gmail.com>
2024-08-14 00:50:21 +0300

    error 1

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,12 @@
-"use client"
+"use client";  
+
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import axios from 'axios';
 import Image from 'next/image';
-import Link from 'next/link'; 
+import Link from 'next/link';
+import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
+
 
 interface Author {
   id: number;
@@ -15,6 +19,7 @@ interface Album {
   name: string;
   coverURL: string;
 }
+
 
 export default function Home() {
   const [authors, setAuthors] = useState<Author[]>([]);
@@ -66,27 +71,41 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href=""
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/next.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
+  <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <div className="fixed bottom-20 left-0 flex h-48 w-full items-end justify-center space-x-2 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+      <button
+      // onClick={()=>router.back()}
+        className="
+        rounded-full
+        bg-purple-500
+        flex
+        items-center
+        justify-center
+        hover:opacity-75
+        transition
+        "
+      >
+        <RxCaretLeft className="text-white" size={35} />
+      </button>
+      <button
+      // onClick={()=>router.forward()}
+        className="
+        rounded-full
+        bg-purple-500
+        flex
+        items-center
+        justify-center
+        hover:opacity-75
+        transition
+        "
+      >
+        <RxCaretRight className="text-white" size={35} />
+      </button>
+    </div>
+    <div className="flex md:hidden gap-x-2 items-center">
+      
+    </div>
+  </div>
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
